b6a0f23d769708da6f2ac998310c24cdeb85e06d
Tashila <tashila.dev@gmail.com>
2021-10-01 19:50:00 +0400

    Initialized json exception filter

--- landon-dotnet-api/Filters/JsonExceptionFilter.cs
+++ landon-dotnet-api/Filters/JsonExceptionFilter.cs
@@ -1,17 +1,33 @@
 using landon_dotnet_api.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Hosting;
 
 namespace landon_dotnet_api.Filters
 {
     public class JsonExceptionFilter : IExceptionFilter
     {
+        private readonly IWebHostEnvironment _env;
+
+        public JsonExceptionFilter(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         public void OnException(ExceptionContext context)
         {
             var error = new ApiError();
 
-            error.Message = context.Exception.Message;
-            error.Detail = context.Exception.StackTrace;
+            if (_env.IsDevelopment())
+            {
+                error.Message = context.Exception.Message;
+                error.Detail = context.Exception.StackTrace;
+            }
+            else
+            {
+                error.Message = "A server error occured.";
+                error.Detail = context.Exception.Message;
+            }
 
             context.Result = new ObjectResult(error)
             {

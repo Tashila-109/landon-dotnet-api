387b12c252d6e7e1263b7be941e59155a2005978
Tashila <tashila.dev@gmail.com>
2021-10-03 10:55:15 +0400

    Initialized info controller

--- landon-dotnet-api/Controllers/InfoController.cs
+++ landon-dotnet-api/Controllers/InfoController.cs
@@ -1,9 +1,26 @@
+using System;
+using landon_dotnet_api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace landon_dotnet_api.Controllers
 {
+    [Route("/[controller]")]
+    [ApiController]
     public class InfoController : ControllerBase
     {
+        private readonly HotelInfo _hotelInfo;
+        public InfoController(IOptions<HotelInfo> hotelInfoWrapper)
+        {
+            _hotelInfo = hotelInfoWrapper.Value;
+        }
         
+        [HttpGet(Name = nameof(GetInfo))]
+        [ProducesResponseType(200)]
+        public ActionResult<HotelInfo> GetInfo()
+        {
+            _hotelInfo.Href = Url.Link(nameof(GetInfo), null);
+            return _hotelInfo;
+        }
     }
 }

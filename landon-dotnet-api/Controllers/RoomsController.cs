103cab1ed6bd23658553efa9171ad8d27cadaa96
Tashila <tashila.dev@gmail.com>
2021-10-04 21:14:13 +0400

    Added in get room by id controller

--- landon-dotnet-api/Controllers/RoomsController.cs
+++ landon-dotnet-api/Controllers/RoomsController.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using landon_dotnet_api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace landon_dotnet_api.Controllers
 {
@@ -8,10 +11,38 @@ namespace landon_dotnet_api.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        private readonly HotelApiDbContext _context;
+
+        public RoomsController(HotelApiDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet(Name = nameof(GetRooms))]
         public IActionResult GetRooms()
         {
             throw new NotImplementedException();
+        }
+
+        [HttpGet("{roomId}", Name = nameof(GetRoomById))]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+        {
+            var entity = await _context.Rooms.SingleOrDefaultAsync(x => x.Id == roomId);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            var resource = new Room
+            {
+                Href = Url.Link(nameof(GetRoomById), new { roomId = entity.Id }),
+                Name = entity.Name,
+                Rate = entity.Rate / 100.0m
+            };
+
+            return resource;
         }
     }
 }

e63d6df2b0f9eda861f1b83309424d9085caa850
Tashila <tashila.dev@gmail.com>
2021-10-06 20:36:34 +0400

    Added in route name and route values

--- landon-dotnet-api/Models/Link.cs
+++ landon-dotnet-api/Models/Link.cs
@@ -1,10 +1,19 @@
 using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace landon_dotnet_api.Models
 {
     public class Link
     {
         private const string GetMethod = "GET";
+
+        public static Link To(string routeName, object routeValues = null) => new Link
+        {
+            RouteName = routeName,
+            RouteValues = routeValues,
+            Method = GetMethod,
+            Relations = null
+        };
         
         public string Href { get; set; }
 
@@ -12,5 +21,13 @@ namespace landon_dotnet_api.Models
 
         [DefaultValue(GetMethod)]
         public string Method { get; set; }
+        
+        // Stores the route name before being rewritten by the LinkRewritingFilter
+        [JsonIgnore]
+        public string RouteName { get; set; }
+        
+        // Stores the route parameters before being rewritten by the LinkRewritingFilter
+        [JsonIgnore]
+        public object RouteValues { get; set; }
     }
 }

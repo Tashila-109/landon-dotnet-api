a2f48c02d8987518ac88db31968843914c4eb6e1
Tashila <tashila.dev@gmail.com>
2021-10-14 22:04:22 +0400

    Updated https class

--- landon-dotnet-api/Filters/RequireHttpsOrCloseAttribute.cs
+++ landon-dotnet-api/Filters/RequireHttpsOrCloseAttribute.cs
@@ -5,7 +5,7 @@ namespace landon_dotnet_api.Filters
 {
     public class RequireHttpsOrCloseAttribute : RequireHttpsAttribute
     {
-        // Handle non https request
+        // Handle non https request context
         protected override void HandleNonHttpsRequest(AuthorizationFilterContext filterContext)
         {
             filterContext.Result = new StatusCodeResult(400);
